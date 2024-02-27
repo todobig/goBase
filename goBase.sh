b88ce2f83e850ccbda0864aeac46d44255c6f222
ToDoBIG <50915354+todobig@users.noreply.github.com>
2024-02-28 02:12:35 +0530

    Update goBase.sh

--- goBase.sh
+++ goBase.sh
@@ -1,45 +1,21 @@
 #!/bin/bash
 
-# Colors for better visualization
-GREEN='\033[0;32m'
-RED='\033[0;31m'
-NC='\033[0m' # No Color
+# Download the latest version of Go
+wget https://go.dev/dl/go1.22.0.linux-amd64.tar.gz
 
-# Remove any existing Go installations
-printf "Removing any existing Go installation...\n"
+# Remove any previous Go installation
 sudo rm -rf /usr/local/go
-sed -i '/\/usr\/local\/go\/bin/d' "$HOME/.profile" # Remove Go bin directory from PATH
 
-# Download and extract Go
-printf "Downloading and extracting Go...\n"
-wget -q https://go.dev/dl/go1.22.0.linux-amd64.tar.gz -O /tmp/go.tar.gz
-sudo tar -C /usr/local -xzf /tmp/go.tar.gz
+# Extract the downloaded archive into /usr/local
+sudo tar -C /usr/local -xzf go1.22.0.linux-amd64.tar.gz
 
 # Add /usr/local/go/bin to the PATH environment variable
-if [[ ":$PATH:" != *":/usr/local/go/bin:"* ]]; then
+if ! grep -qF "/usr/local/go/bin" "$HOME/.profile"; then
     echo 'export PATH=$PATH:/usr/local/go/bin' >> "$HOME/.profile"
-    source "$HOME/.profile"
 fi
 
-# Verify Go installation
-go_version=$(go version)
-if [[ $go_version == *"go version"* ]]; then
-    printf "${GREEN}Go is installed. Version:${NC}\n"
-    echo "$go_version"
-else
-    printf "${RED}Go installation failed.${NC}\n"
-    exit 1
-fi
+# Apply changes to the current shell
+source "$HOME/.profile"
 
-# Test program
-printf "Testing Go installation with a sample program...\n"
-cat <<EOF > hello.go
-package main
-
-import "fmt"
-
-func main() {
-    fmt.Println("Hello, Go!")
-}
-EOF
-go run hello.go
+# Verify the installation
+go version
